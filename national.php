c939e5d98a7bad5bec7e09a12e2a589f2f12253f
Yeri <yeri@tiete.be>
2010-06-24 16:13:56 +0200

    favicon, htaccess

--- national.php
+++ national.php
@@ -65,6 +65,7 @@ default:		$txt_from = "From:";
 <head>
 <link href="css/mobile.css" rel="stylesheet" type="text/css" />
 <link rel="apple-touch-icon" href="./img/irail.png" />
+<link rel="shortcut icon" type="image/x-icon" href="./img/favicon.ico">
 <meta name="viewport" content="width=320; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
 <meta name="keywords" content="nmbs, sncb, iphone, mobile, irail, irail.be, route planner">
 <meta name="language" content="en"> 
