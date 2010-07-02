8afbd273049418c0bf6901baa6d0e6ff40d3e608
Yeri <yeri@tiete.be>
2010-07-02 14:40:33 +0200

    changelog

--- docs/changelog.php
+++ docs/changelog.php
@@ -1,6 +1,6 @@
 <?php
 $v = `cd .. && git rev-parse --short HEAD`;
 $c = `cd .. && git whatchanged`;
-echo "<pre>" . $v . "</pre><br /><br /><br />";
+echo "<pre>Git version: " . $v . "</pre><br /><br />Changelog: <br />";
 echo "<pre>" . $c . "</pre>";
 ?>
