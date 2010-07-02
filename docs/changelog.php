dcba1bc29cf77397dc8928eee5d1acfa54ecd919
Yeri <yeri@tiete.be>
2010-07-02 17:39:45 +0200

    changelog changes

--- docs/changelog.php
+++ docs/changelog.php
@@ -3,7 +3,7 @@ include '../includes/getVersion.php';
 include '../includes/getChangelog.php';
 
 $irailChangelog = str_replace("<", "&#060;", $irailChangelog);
-$irailChangelog = str_replace(">", "&#061;", $irailChangelog);
+$irailChangelog = str_replace(">", "&#062;", $irailChangelog);
 
 echo "<pre>Git version: " . $irailVersion . "</pre>";
 echo "<pre>Git changelog: <br /><br />" . $irailChangelog . "</pre>";
