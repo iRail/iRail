25a7cdea509db83fd4e7b5805852bf5654f41573
Yeri <yeri@tiete.be>
2010-07-02 16:55:13 +0200

    UA file fix

--- includes/getUA.php
+++ includes/getUA.php
@@ -1,5 +1,5 @@
 <?php
 include 'getVersion.php';
-$irailAgent = "iRail.be by Yeri \"Tuinslak\" Tiete (yeri.be); Git version $irailVersion";
+$irailAgent = "iRail.be by Yeri 'Tuinslak' Tiete (yeri.be); Git version " . $irailVersion;
 ?>
 
