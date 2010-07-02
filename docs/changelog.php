4c59eb413af589ab77d074357e15a5c35ece3bd9
Yeri <yeri@tiete.be>
2010-07-02 14:39:43 +0200

    changelog

--- docs/changelog.php
+++ docs/changelog.php
@@ -1,4 +1,6 @@
 <?php
-$c = `cd ../ && git whatchanged`;
+$v = `cd .. && git rev-parse --short HEAD`;
+$c = `cd .. && git whatchanged`;
+echo "<pre>" . $v . "</pre><br /><br /><br />";
 echo "<pre>" . $c . "</pre>";
 ?>
