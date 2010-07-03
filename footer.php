b99f7300d95c0cf92f96ab550c331548d62ebac2
Yeri <yeri@tiete.be>
2010-07-03 03:43:49 +0200

    footer edit; include version

--- footer.php
+++ footer.php
@@ -5,6 +5,5 @@ include 'includes/getVersion.php';
 data retrieved from <a href="http://www.b-rail.be/" target="_blank">(B)</a>; 
 Version: 
 <?php 
-$irailVersion = str_replace(" ", "", $irailVersion);
-echo $irailVersion; 
-?>.</p>
+echo $irailVersion . "."; 
+?></p>
