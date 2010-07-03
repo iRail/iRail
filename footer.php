bb02d3458cd3099a6db4c412b152f277e34f4d32
Yeri <yeri@tiete.be>
2010-07-03 21:12:06 +0200

    footer edit

--- footer.php
+++ footer.php
@@ -3,9 +3,9 @@ include 'includes/getVersion.php';
 ?>
 <p style="color:#999999;font-size:12px;position:absolute;left:10px;">made by <a href="http://yeri.be/irail/" target="_blank">Yeri Tiete</a>; 
 data from <a href="http://www.b-rail.be/" target="_blank">(B)</a>; 
-version: 
+version: <a href="changelog">
 <?php 
 $irailVersion = str_replace("\n", "", $irailVersion);
 
 echo $irailVersion; 
-?>.</p>
+?></a>.</p>
