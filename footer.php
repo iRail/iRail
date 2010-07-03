85c00afa2cfae07759ae69a2f7d6498706ed6481
Yeri <yeri@tiete.be>
2010-07-03 16:33:55 +0200

    version layout fix

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@ include 'includes/getVersion.php';
 ?>
 <p style="color:#999999;font-size:12px;position:absolute;left:10px;">made by <a href="http://yeri.be/irail/" target="_blank">Yeri Tiete</a>; 
 data retrieved from <a href="http://www.b-rail.be/" target="_blank">(B)</a>; 
-Version: 
+version: 
 <?php 
 $irailVersion = str_replace("\n", "", $irailVersion);
 
