0931f6d0474a0df08418bc3544c308b42125580e
Yeri <yeri@tiete.be>
2010-07-03 02:38:05 +0200

    query fixes

--- iquery.php
+++ iquery.php
@@ -24,7 +24,7 @@ setcookie("intto", $_POST['to'], time()+60*60*24*360);
 $request_options = array(
 			referer => "http://irail.be/", 
 			timeout => "30",
-			//useragent => $irailAgent, 
+			useragent => "$irailAgent", 
 		);
 			
 // get lang from cookie
