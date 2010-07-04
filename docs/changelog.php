456e73d974bb8c3a3e8966290462e59cb7ef0685
Yeri <yeri@tiete.be>
2010-07-04 02:34:19 +0200

    layout

--- docs/changelog.php
+++ docs/changelog.php
@@ -21,8 +21,8 @@ include '../includes/getChangelog.php';
 $irailChangelog = str_replace("<", "&#060;", $irailChangelog);
 $irailChangelog = str_replace(">", "&#062;", $irailChangelog);
 
-echo "<pre>Git version: " . $irailVersion . "</pre>";
-echo "<pre>Git changelog: <br /><br />" . $irailChangelog . "</pre>";
+echo "<pre>iRail Git version: " . $irailVersion . "</pre>";
+echo "<pre>iRail Git changelog: <br /><br />" . $irailChangelog . "</pre>";
 ?>
 
 <?php
