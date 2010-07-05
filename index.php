0b39f5df0f4a379a09653ab4a9872cb49f258c42
Yeri <yeri@tiete.be>
2010-07-05 22:08:21 +0200

    dev mode

--- index.php
+++ index.php
@@ -11,6 +11,17 @@
 // other = output msg 
 $maintenance = 0;
 
+// set dev mode on/off
+// if dev is false, redirect to http://irail.be 
+$dev = 0;
+
+// get current domain
+$domain = str_replace("www.","",$_SERVER['HTTP_HOST']);
+
+if($dev == 0 && && $domain != "irail.be" && $domain != "irail.nl") {
+	header('Location: http://irail.be');
+}
+
 if($maintenance == 0) {
 	// edit URL to match domain/site
 	header('Location: national');
