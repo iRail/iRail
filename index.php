065e7472901659a691708cd5ce1138b502b88678
Yeri <yeri@tiete.be>
2010-07-05 23:31:08 +0200

    dev mode

--- index.php
+++ index.php
@@ -18,10 +18,9 @@ $dev = 0;
 // get current domain
 $domain = str_replace("www.","",$_SERVER['HTTP_HOST']);
 
-echo $domain;
-
 if($dev == 0 && $domain != "irail.be" && $domain != "irail.nl") {
 	header('Location: http://irail.be');
+	return;
 }
 
 if($maintenance == 0) {
