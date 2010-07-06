ee4631e18a531f1b819750ef57e549587e6795e1
Yeri <yeri@tiete.be>
2010-07-06 17:29:08 +0200

    test

--- test.php
+++ test.php
@@ -22,8 +22,9 @@ $url = "http://hari.b-holding.be/hafas/bin/query.exe/en?";
 	
 	$body = http_parse_message($post)->body;
 	
+	// http://hari.b-rail.be/HAFAS/bin/query.exe/nn?seqnr=31&ident=k4.0538412.1278427697&OK#focus&REQ0HafasSearchForw=1&REQ0JourneyDate=Di%2C%2006%2F07%2F10&REQ0JourneyDest_Bike_enable=0&REQ0JourneyDest_KissRide_enable=0&REQ0JourneyProduct_prod_list=1%3A0111111000000000&REQ0JourneyStopsS0A=1&REQ0JourneyStopsS0K=S-0N1&REQ0JourneyStopsZ0A=1&REQ0JourneyStopsZ0K=S-1N1&REQ0JourneyTime=15%3A20&queryPageDisplayed=yes&start=Bevestig&wDayExt0=Ma%7CDi%7CWo%7CDo%7CVr%7CZa%7CZo
 	
-$ch = curl_init("http://hari.b-rail.be/HAFAS/bin/query.exe/nn?seqnr=1&ident=k4.0538412.1278427697&OK#focus");
+$ch = curl_init("http://hari.b-rail.be/HAFAS/bin/query.exe/nn?seqnr=31&ident=k4.0538412.1278427697&OK#focus&REQ0HafasSearchForw=1&REQ0JourneyDate=Di%2C%2006%2F07%2F10&REQ0JourneyDest_Bike_enable=0&REQ0JourneyDest_KissRide_enable=0&REQ0JourneyProduct_prod_list=1%3A0111111000000000&REQ0JourneyStopsS0A=1&REQ0JourneyStopsS0K=S-0N1&REQ0JourneyStopsZ0A=1&REQ0JourneyStopsZ0K=S-1N1&REQ0JourneyTime=15%3A20&queryPageDisplayed=yes&start=Bevestig&wDayExt0=Ma%7CDi%7CWo%7CDo%7CVr%7CZa%7CZo");
 curl_setopt($ch, CURLOPT_POST      ,1);
 curl_setopt($ch, CURLOPT_POSTFIELDS    , "");
 curl_setopt($ch, CURLOPT_COOKIE, "__utma=1.1333636757.1252318191.1254576431.1255378610.7; __utmz=1.1278422421.7.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __utmc=1; __utmb=1.1.10.1278427684");
