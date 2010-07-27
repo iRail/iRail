6dd042b58eafbc1472334136e2cb89eb7bf29654
Yeri <yeri@tiete.be>
2010-07-27 13:30:30 +0200

    GA

--- includes/googleAnalytics.php
+++ includes/googleAnalytics.php
@@ -1,4 +1,4 @@
-<!-- Google Analytics -->
+<!-- Google Analytics // please do not change the UA-code unless hosting on your own webspace. Don't commit changes to this file. -->
 <script type="text/javascript">
 var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
 document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
