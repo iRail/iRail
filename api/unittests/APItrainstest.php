fa1077d366078bbe252516a7b6738dbb8d84a4fe
pieterc <pieterc@pieterc-laptop>
2010-08-10 09:13:19 +0200

    [unit test] - if testing from and to are the same, please continue ;-)

--- api/unittests/APItrainstest.php
+++ api/unittests/APItrainstest.php
@@ -4,17 +4,20 @@ include "../../includes/stationlist.php";
 echo "failures for ".date("d-m-y H:i"). ":<br/>";
 $failures = 0;
 foreach($stations as $i => $value) {
-        $ch = curl_init();
-        $station2 = urlencode($value);
-        $connectionurl = "http://dev.irail.be/api/trains.php?from=harelbeke&to=" . $station2;
-        curl_setopt($ch, CURLOPT_URL, $connectionurl);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        $output = curl_exec($ch);
-        if(preg_match("/error/si", $output)){
-            $failures ++;
-            echo $failures . ". " . $value . "<br/>";
-        }
-        curl_close($ch);
+    if($value == "HARELBEKE") {
+        continue;
+    }
+    $ch = curl_init();
+    $station2 = urlencode($value);
+    $connectionurl = "http://dev.irail.be/api/trains.php?from=harelbeke&to=" . $station2;
+    curl_setopt($ch, CURLOPT_URL, $connectionurl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+    if(preg_match("/error/si", $output)) {
+        $failures ++;
+        echo $failures . ". " . $value . "<br/>";
+    }
+    curl_close($ch);
 }
 
 echo "total failures: " .$failures . "\n";
