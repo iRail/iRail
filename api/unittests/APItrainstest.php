e98b9d665b1fd07f86bd0d2c68e66df2bd2460bb
pieterc <pieterc@pieterc-laptop>
2010-08-10 08:16:41 +0200

    url encoded station2 in test page

--- api/unittests/APItrainstest.php
+++ api/unittests/APItrainstest.php
@@ -5,7 +5,8 @@ echo "failures for ".date("d-m-y H:i"). ":<br/>";
 $failures = 0;
 foreach($stations as $i => $value) {
         $ch = curl_init();
-        $connectionurl = "http://dev.irail.be/api/trains.php?from=harelbeke&to=" . $value;
+        $station2 = urlencode($value);
+        $connectionurl = "http://dev.irail.be/api/trains.php?from=harelbeke&to=" . $station2;
         curl_setopt($ch, CURLOPT_URL, $connectionurl);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         $output = curl_exec($ch);
