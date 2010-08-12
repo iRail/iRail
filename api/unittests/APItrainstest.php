edca9d209fbb64f78b3934933401d17b9386325d
pieterc <pieterc@pieterc-laptop>
2010-08-12 21:35:18 +0200

    commented out unittest for safety reasons

--- api/unittests/APItrainstest.php
+++ api/unittests/APItrainstest.php
@@ -1,24 +1,24 @@
 <?php
 include "../../includes/stationlist.php";
-
-echo "failures for ".date("d-m-y H:i"). ":<br/>";
-$failures = 0;
-foreach($stations as $i => $value) {
-    if($value == "HARELBEKE") {
-        continue;
-    }
-    $ch = curl_init();
-    $station2 = urlencode($value);
-    $connectionurl = "http://dev.irail.be/api/trains.php?from=harelbeke&to=" . $station2;
-    curl_setopt($ch, CURLOPT_URL, $connectionurl);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $output = curl_exec($ch);
-    if(preg_match("/error/si", $output)) {
-        $failures ++;
-        echo $failures . ". " . $value . "<br/>";
-    }
-    curl_close($ch);
-}
-
-echo "total failures: " .$failures . "\n";
+//for safety reasons, commented
+//echo "failures for ".date("d-m-y H:i"). ":<br/>";
+//$failures = 0;
+//foreach($stations as $i => $value) {
+//    if($value == "HARELBEKE") {
+//        continue;
+//    }
+//    $ch = curl_init();
+//    $station2 = urlencode($value);
+//    $connectionurl = "http://dev.irail.be/api/trains.php?from=harelbeke&to=" . $station2;
+//    curl_setopt($ch, CURLOPT_URL, $connectionurl);
+//    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+//    $output = curl_exec($ch);
+//    if(preg_match("/error/si", $output)) {
+//        $failures ++;
+//        echo $failures . ". " . $value . "<br/>";
+//    }
+//    curl_close($ch);
+//}
+//
+//echo "total failures: " .$failures . "\n";
 ?>
