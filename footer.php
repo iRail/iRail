597542b11bffa50652a05e0dcd4421c582458509
Yeri <yeri@tiete.be>
2010-08-12 20:25:10 +0200

    about page, but not happy with layout :<

--- footer.php
+++ footer.php
@@ -23,7 +23,7 @@
 
 include 'includes/getVersion.php';
 ?>
-<p style="color:#999999;font-size:12px;position:absolute;left:10px;">made by <a href="http://yeri.be/irail/" target="_blank">Yeri Tiete</a>; 
+<p style="color:#999999;font-size:12px;position:absolute;left:10px;">made by <a href="about">iRail</a>; 
 data from <a href="http://www.b-rail.be/" target="_blank">(B)</a>; 
 version: <a href="changelog">
 <?php 
