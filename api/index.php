d23a37fa8deb75a720aad91e9627a5a7703504ba
Yeri Tiete <yeri@tiete.be>
2010-09-25 19:47:58 +0200

    api index

--- api/index.php
+++ api/index.php
@@ -12,15 +12,16 @@
       <h1>Usage</h1>
       <p><a href="http://project.irail.be/cgi-bin/trac.fcgi/wiki/APIv1Draft">here</a></p>
       <h1>Grab the code</h1>
-      <p>The code of this website is free and open source. It is licensed under the GPL v 3 and you can find it at github: </p>
+      <p>The code of this website is free and open source. It is licensed under the GPL v3 and you can find it at Github: </p>
       <p><a href="http://github.com/Tuinslak/iRail/">Github.com/Tuinslak/iRail</a></p>
       <h1>Authors</h1>
-      <p>Copyright &copy; 2008, 2009, 2010 Yeri Tiete ("<a href="http://yeri.be" target="_blank">Tuinslak</a>") - yeri@irail.be</p>
-      <p>Copyright &copy; 2010 Pieter Colpaert (<a href="http://bonsansnom.wordpress.com" target="_blank">http://bonsansnom.wordpress.com<a/>) - pieter@irail.be</p>
-      <p><a href="http://project.irail.be/cgi-bin/trac.fcgi/wiki/Contributors">and others</a></p>
+      <p><a href="http://project.irail.be">Project iRail</a></a>
+      <p>Copyright &copy; 2008, 2009, 2010 Yeri Tiete ("<a href="http://yeri.be" target="_blank">Tuinslak</a>") - yeri@irail.be,</p>
+      <p>Copyright &copy; 2010 Pieter Colpaert (<a href="http://bonsansnom.wordpress.com" target="_blank">http://bonsansnom.wordpress.com<a/>) - pieter@irail.be,</p>
+      <p>And <a href="http://project.irail.be/cgi-bin/trac.fcgi/wiki/Contributors">others</a>.</p>
       <h1>Bugs</h1>
       <p>Nobody is perfect. Writing a webscraper is not the most fault tolerant system you would ever write. Since we think sooner or later NMBS will release an API of their own, we didn't mind writing clean code. So before you dive in, don't mind the mess.</p>
-      <p>On the other hand if you noticed a flaw, something that doesn't work, something that might be important, ... feel free to report an issue at <a href="http://project.irail.be">the project iRail site</a></p>
+      <p>On the other hand if you noticed a flaw, something that doesn't work, something that might be important, ... feel free to report a new ticket at the <a href="http://project.irail.be/cgi-bin/trac.fcgi/newticket">project iRail</a> site.</p>
       <p><br/><i> Thanks for your interest and happy hacking :-)</i><br/><br/>
           <i>Some rights reserved - CC By-Sa - <img src="http://mirrors.creativecommons.org/presskit/buttons/88x31/png/by-sa.png" width="88" height="33" alt="Creative Commons Attribution, Share alike"/></i>
       </p>
