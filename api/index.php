289b614cbecfd99a528fff5737446a1f91a643d6
Yeri Tiete <yeri@tiete.be>
2010-09-25 19:44:53 +0200

    api index

--- api/index.php
+++ api/index.php
@@ -8,15 +8,15 @@
   </head>
   <body>
       <h1>Welcome developer</h1>
-      <p>This is the unofficial API for the SNCB/NMBS in Belgium. If you want to implement train schedules in your application you're at the right place.</p>
+      <p>This is the unofficial API for the SNCB/NMBS in Belgium. If you want to implement train schedules in your application you are at the right place.</p>
       <h1>Usage</h1>
       <p><a href="http://project.irail.be/cgi-bin/trac.fcgi/wiki/APIv1Draft">here</a></p>
       <h1>Grab the code</h1>
       <p>The code of this website is free and open source. It is licensed under the GPL v 3 and you can find it at github: </p>
-      <p><a href="http://github.com/Tuinslak/iRail/">here (http://github.com/Tuinslak/iRail/)</a></p>
+      <p><a href="http://github.com/Tuinslak/iRail/">Github.com/Tuinslak/iRail</a></p>
       <h1>Authors</h1>
-      <p>Copyright &copy; 2008, 2009, 2010 Yeri "Tuinslak" Tiete (http://yeri.be)</p>
-      <p>Copyright &copy; 2010 Pieter Colpaert (http://bonsansnom.wordpress.com) - pieter@irail.be</p>
+      <p>Copyright &copy; 2008, 2009, 2010 Yeri Tiete ("<a href="http://yeri.be" target="_blank">Tuinslak</a>") - yeri@irail.be</p>
+      <p>Copyright &copy; 2010 Pieter Colpaert (<a href="http://bonsansnom.wordpress.com" target="_blank">http://bonsansnom.wordpress.com<a/>) - pieter@irail.be</p>
       <p><a href="http://project.irail.be/cgi-bin/trac.fcgi/wiki/Contributors">and others</a></p>
       <h1>Bugs</h1>
       <p>Nobody is perfect. Writing a webscraper is not the most fault tolerant system you would ever write. Since we think sooner or later NMBS will release an API of their own, we didn't mind writing clean code. So before you dive in, don't mind the mess.</p>
