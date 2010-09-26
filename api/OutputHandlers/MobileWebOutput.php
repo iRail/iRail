f4fc6ae7ed1f167a9c475e0e210c53bf05fc16fb
Pieter Colpaert <pieter@irail.be>
2010-09-26 16:53:10 +0200

    new icon in html

--- api/OutputHandlers/MobileWebOutput.php
+++ api/OutputHandlers/MobileWebOutput.php
@@ -62,7 +62,8 @@ function hideAddressBar() { window.scrollTo(0, 1); }
         $index = 0;
         foreach($this->connections as $con){
             $output .= "<tr class=\"color". $index%2 ."\">";
-            $output .= "<td>". '<a href="http://maps.google.be/?saddr=Station '. $con -> getDepart() -> getStation() -> getName() . '&daddr=Station '. $con -> getArrival() -> getStation() -> getName() . '" target="_blank"><img border="0" class="icon" src="/HAFAS/img/icon_map.gif" width="14" height="14" alt="Local Map" /></a>' . "</td>";
+            $output .= "<!-- compass image by Yusuke Kamiyamane, Creative Commons (Attribution 3.0 Unported)-->";
+            $output .= "<td>". '<a href="http://maps.google.be/?saddr=Station '. $con -> getDepart() -> getStation() -> getName() . '&daddr=Station '. $con -> getArrival() -> getStation() -> getName() . '" target="_blank"><img border="0" class="icon" src="img/map.png" width="14" height="14" alt="Local Map" /></a>' . "</td>";
             $output .= "<td>". $con -> getDepart() -> getStation() -> getName() . "<br/>". $con -> getArrival() -> getStation() -> getName() . "</td>";
             $output .= "<td>" . date("d/m/y", $con -> getDepart() -> getTime()) . "<br/>". date("d/m/y", $con -> getArrival() -> getTime()) ."</td>";
             $output .= "<td>" . date("H:i", $con -> getDepart() -> getTime()) . "<br/>". date("H:i", $con -> getArrival() -> getTime()) ."</td>";
