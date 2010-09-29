507d1beb00ed15c3408fca5ef21b0716319854c9
Pieter Colpaert <pieter@irail.be>
2010-09-29 15:39:20 +0200

    More compact view for iRail mobile website

--- api/OutputHandlers/MobileWebOutput.php
+++ api/OutputHandlers/MobileWebOutput.php
@@ -14,7 +14,7 @@ class MobileWebOutput extends ConnectionOutput {
     }
 
     public function printAll() {
-        $this->printHeader();        
+        $this->printHeader();
         $this->printBody();
     }
 
@@ -36,13 +36,23 @@ function hideAddressBar() { window.scrollTo(0, 1); }
 </head><body>';
     }
 
-    private function printBody(){
+    private function printBody() {
+        $con = $this->connections[0];
         echo '<body>
+<!-- compass image by Yusuke Kamiyamane, Creative Commons (Attribution 3.0 Unported) -->
+<table>
+<tr><td>
+Map: <a href="http://maps.google.be/?saddr=Station'. $con -> getDepart() -> getStation() -> getName().'&daddr=Station '. $con -> getArrival() -> getStation() -> getName() . '" target="_blank"><img border="0" class="icon" src="img/map.png" width="14" height="14" alt="Local Map" /></a>
+</td>
+<td>
+Date: '.date("d/m/y", $con -> getDepart() -> getTime()) .'
+    </td>
+    </tr>
+    <tr><td>
+From: '.$con -> getDepart() -> getStation() -> getName() .'</td><td>
+To: '. $con -> getArrival() -> getStation() -> getName() . '</td></tr></table>
 <table align="left" cellpadding="0" cellspacing="1" bgcolor="FFFFFF" summary="Train Info">
 <tr>
-<th>Map</th>
-<th>Station </th>
-<th>Date </th>
 <th>Time </th>
 <th>Duration </th>
 <th>Delay</th>
@@ -50,27 +60,23 @@ function hideAddressBar() { window.scrollTo(0, 1); }
 <th>Transportation</th>
 </tr>
 '. $this->getConnectionsOutput();
-    echo "<tr><td colspan=\"8\"><center><form name=\"return\" method=\"post\" action=\"national\"><input type=\"submit\" name=\"submit\" value=\"Back\"></center><br /></td></tr>";
-    echo "</table>";
-    echo '</body>
+        echo "<tr><td colspan=\"8\"><center><form name=\"return\" method=\"post\" action=\"national\"><input type=\"submit\" name=\"submit\" value=\"Back\"></center><br /></td></tr>";
+        echo "</table>";
+        echo '</body>
         </html>';
     }
 
-    private function getConnectionsOutput(){
+    private function getConnectionsOutput() {
         date_default_timezone_set("Europe/Brussels");
         $output= "";
         $index = 0;
-        foreach($this->connections as $con){
+        foreach($this->connections as $con) {
             $output .= "<tr class=\"color". $index%2 ."\">";
-            $output .= "<!-- compass image by Yusuke Kamiyamane, Creative Commons (Attribution 3.0 Unported) -->";
-            $output .= "<td>". '<a href="http://maps.google.be/?saddr=Station '. $con -> getDepart() -> getStation() -> getName() . '&daddr=Station '. $con -> getArrival() -> getStation() -> getName() . '" target="_blank"><img border="0" class="icon" src="img/map.png" width="14" height="14" alt="Local Map" /></a>' . "</td>";
-            $output .= "<td>". $con -> getDepart() -> getStation() -> getName() . "<br/>". $con -> getArrival() -> getStation() -> getName() . "</td>";
-            $output .= "<td>" . date("d/m/y", $con -> getDepart() -> getTime()) . "<br/>". date("d/m/y", $con -> getArrival() -> getTime()) ."</td>";
             $output .= "<td>" . date("H:i", $con -> getDepart() -> getTime()) . "<br/>". date("H:i", $con -> getArrival() -> getTime()) ."</td>";
 
             $minutes = $con -> getDuration()/60 % 60;
             $hours = floor($con -> getDuration() / 3600);
-            if($minutes < 10){
+            if($minutes < 10) {
                 $minutes = "0" . $minutes;
             }
             $output .= "<td>" . $hours. ":" . $minutes ."</td>";
@@ -87,9 +93,9 @@ function hideAddressBar() { window.scrollTo(0, 1); }
         return $output;
     }
 
-    private function getTrains(Connection $con){
+    private function getTrains(Connection $con) {
         $out = "";
-        foreach($con -> getVias() as $v){
+        foreach($con -> getVias() as $v) {
             $out .= $v -> getVehicle() -> getInternalId() . "<br/>";
         }
         $out .= $con -> getArrival() -> getVehicle() -> getInternalId();
