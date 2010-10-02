c0c2cbd1e27213e1d43a964a2692fa48c69140b6
Yeri Tiete <yeri@tiete.be>
2010-10-02 19:16:01 +0200

    minor updates

--- docs/api/db/apilog.sql
+++ docs/api/db/apilog.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.2.2.1
+-- version 2.11.8.1deb5+lenny5
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 19, 2010 at 07:24 PM
--- Server version: 5.0.84
--- PHP Version: 5.3.3-pl0-gentoo
+-- Generation Time: Oct 02, 2010 at 06:52 PM
+-- Server version: 5.0.51
+-- PHP Version: 5.2.6-1+lenny9
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
@@ -25,8 +25,7 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- Table structure for table `apilog`
 --
 
-DROP TABLE IF EXISTS `apilog`;
-CREATE TABLE `apilog` (
+CREATE TABLE IF NOT EXISTS `apilog` (
   `id` int(11) NOT NULL auto_increment,
   `time` varchar(40) NOT NULL COMMENT 'date in rfc2822 format',
   `useragent` varchar(200) default NULL COMMENT 'browser info',
@@ -35,12 +34,9 @@ CREATE TABLE `apilog` (
   `errors` varchar(100) default 'none' COMMENT 'Error field',
   `ip` varchar(20) default NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 --
 -- Dumping data for table `apilog`
 --
 
-INSERT INTO `apilog` VALUES(1, 'Sun, 19 Sep 2010 17:19:56 +0000', 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; en-US) AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.472.62 Safari/534.3', 'Brugge', 's gravenbla ', 'none', NULL);
-INSERT INTO `apilog` VALUES(2, 'Sun, 19 Sep 2010 19:20:30 +0200', 'Java0', 'Bruxelles-Midi', 'Liege-Guillemins', 'none', NULL);
-INSERT INTO `apilog` VALUES(3, 'Sun, 19 Sep 2010 17:22:00 +0000', 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; en-US) AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.472.62 Safari/534.3', 'Brugge', 's gravenbla ', 'none', NULL);
