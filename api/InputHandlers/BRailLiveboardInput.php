caa80a25bcc62539395e1e77e16db03014e42fb9
Pieter Colpaert <pieter@irail.be>
2010-10-24 00:56:40 +0200

    Added liveboard code - Work in progress

--- api/InputHandlers/BRailLiveboardInput.php
+++ api/InputHandlers/BRailLiveboardInput.php
@@ -1,31 +1,67 @@
 <?php
 /**
- * Description of BRailLiveboardInput
+ * Old fashioned scraper of railtime
  *
  * @author pieterc
  */
 ini_set("include_path", ".:../:api/DataStructs:DataStructs:../includes:includes");
 include_once("LiveboardInput.php");
-class BRailLiveboardInput extends LiveboardInput{
-    private $scrapeUrl = "http://www.railtime.be/mobile/SearchStation.aspx";
-    public function __construct() {
+include_once("DataStructs/Station.php");
+include_once("DataStructs/TripNode.php");
 
-    }
-
+class BRailLiveboardInput extends LiveboardInput{ 
+    private $arrdep;
+    private $name;
     protected function fetchData(Request $request) {
         include "getUA.php";
         $url="http://hari.b-rail.be/Hafas/bin/extxml.exe";
+        $scrapeUrl = "http://www.railtime.be/mobile/SearchStation.aspx";
         $request_options = array(
                 "referer" => "http://api.irail.be/",
                 "timeout" => "30",
                 "useragent" => $irailAgent,
         );
+        $stationname = strtoupper($request -> getStation());
+        include("includes/railtimeids.php");
+        $rtid = $railtimeids[$stationname];
+        $this -> arrdep = $request -> getArrdep();
+        $this-> name = $request -> getStation();
+        $scrapeUrl .= "?l=". $request -> getLang() . "&s=1&sid=". $rtid ."&tr=22:15-60&da=". substr($request -> getArrdep(), 0,1) ."&p=2";
+        $post = http_post_data($scrapeUrl, "", $request_options) or die("");
+        $body = http_parse_message($post)->body;
 
-
+        return $body;
     }
 
     protected function transformData($serverData) {
 
+        //echo $serverData;
+        $name = $this -> name;
+        $locationX = 0; //todo
+        $locationY = 0;
+        $station = new Station($name, $locationX, $locationY);
+        $arrdep = $this -> arrdep;
+        $nodes = array();
+        preg_match_all("/<td valign=\"top\">(.*?)<\/td>/ism", $serverData, $matches);
+        $i = 0;
+        foreach($matches[1] as $td){
+            if($i == 0){
+                $i++;
+                continue;
+            }
+//TODO:<a href="/mobile/SearchTrain.aspx?l=EN&s=1&tid=2043&da=D&p=2">22:20</a></td><td valign="top">&nbsp;<font color="Red">Changed</font>&nbsp;Charleroi-Sud&nbsp;<span>[IC2043&nbsp;Track&nbsp;<font color="Red">12</font>]</span>&nbsp;<img src="/mobile/images/Work.png" border="0" /><br/>
+            $station = new Station($name, $locationX, $locationY);
+            $vehicle = "ohjo";
+            $unixtime = 0;
+            $delay = 0;
+            $platform = 1;
+            $platformNormal = "yes";
+            $nodes[$i-1] = new TripNode($platform, $delay, $unixtime, $station, $vehicle, $platformNormal);
+            $i++;
+        }
+
+        $liveboard = new Liveboard($station, $arrdep, $nodes);
+        return $liveboard;
     }
 
 }
--- api/OutputHandlers/LiveboardOutput.php
+++ api/OutputHandlers/LiveboardOutput.php
@@ -1,13 +1,51 @@
 <?php
+
 /**
  * Description of LiveboardOutput
  *
  * @author pieterc
  */
 include_once("Output.php");
-abstract class LiveboardOutput implements Output{
-    protected function buildXML($liveboard){
-        
+include_once("DataStructs/Liveboard.php");
+
+abstract class LiveboardOutput implements Output {
+
+    protected function buildXML($liveboard) {
+        $xml = new DOMDocument("1.0", "UTF-8");
+        $rootNode = $xml->createElement("liveboard");
+        $rootNode->setAttribute("version", "1.0");
+        $rootNode->setAttribute("timestamp", date("U"));
+        $xml->appendChild($rootNode);
+        $liveboard->getStation();
+        $station = $xml->createElement("station", $liveboard->getStation()->getName());
+        //provide also this tag for old versions
+        $station->setAttribute("location", $liveboard->getStation()->getY() . " " . $liveboard->getStation()->getX());
+        //new version
+        $station->setAttribute("locationY", $liveboard->getStation()->getY());
+        $station->setAttribute("locationX", $liveboard->getStation()->getX());
+        $rootNode->appendChild($station);
+        foreach ($liveboard->getNodes() as $node) {
+            $nodeEl = null;
+            if ($liveboard->getDeparr() == "ARR") {
+                $nodeEl = $xml->createElement("arrival");
+            } else if ($liveboard->getDeparr() == "DEP") {
+                $nodeEl = $xml->createElement("departure");
+            }
+            $nodeEl ->setAttribute("delay", $node -> getDelay() );
+            $station = $xml->createElement("station", $node->getStation()->getName());
+            //provide also this tag for old versions
+            $station->setAttribute("location", $node->getStation()->getY() . " " . $node->getStation()->getX());
+            //new version
+            $station->setAttribute("locationY", $node->getStation()->getY());
+            $station->setAttribute("locationX", $node->getStation()->getX());
+            $platform = $xml -> createElement("platform",$node -> getPlatform());
+            $nodeEl->appendChild($platform);
+            $nodeEl->appendChild($station);
+
+            $rootNode -> appendChild($nodeEl);
+        }
+        return $xml;
     }
+
 }
 ?>
