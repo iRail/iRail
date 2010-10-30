e5460abac2d6afe78bb9bfe4a760c113c6687cdd
Pieter Colpaert <pieter@irail.be>
2010-10-30 10:13:34 +0200

    added NS Liveboard input and NS Vehicle input. For now they do the same as the BRail ones

---
+++ api/InputHandlers/NSLiveboardInput.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Old fashioned scraper of railtime
+ *
+ * @author pieterc
+ */
+include_once("BRailLiveboardInput.php");
+
+class NSLiveboardInput extends BRailLiveboardInput {
+
+}
+?>
