e5f74d909e7403e2fb769159dd84d394c440a8e1
Pieter Colpaert <pieter@irail.be>
2010-12-01 17:29:32 +0100

    updated index file of the API

--- api/index.php
+++ api/index.php
@@ -2,21 +2,23 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>iRail.be API</title>
+    <title>iRail API</title>
   </head>
   <body>
       <h1>Welcome developer</h1>
-      <p>This is the unofficial API for the SNCB/NMBS in Belgium. If you want to implement train schedules in your application you are at the right place.</p>
+      <p>Welcome to the iRail API. If you want to implement train (more to come later) schedules in your application you are at the right place.</p>
       <h1>Usage</h1>
-      <p><a href="http://project.irail.be/cgi-bin/trac.fcgi/wiki/APIv1Draft">here</a></p>
+      <p><a href="http://project.irail.be/cgi-bin/trac.fcgi/wiki/APIv1">here</a></p>
       <h1>Grab the code</h1>
       <p>The code of this website is free and open source. It is licensed under the GPL v3 and you can find it at Github: </p>
-      <p><a href="http://github.com/Tuinslak/iRail/">Github.com/Tuinslak/iRail</a></p>
+      <p><a href="http://github.com/iRail/">Github.com/iRail</a></p>
       <h1>Authors</h1>
       <p><a href="http://project.irail.be">Project iRail</a>
-      <p>Copyright &copy; 2008, 2009, 2010 Yeri Tiete ("<a href="http://yeri.be" target="_blank">Tuinslak</a>") - yeri@irail.be,</p>
-      <p>Copyright &copy; 2010 Pieter Colpaert (<a href="http://bonsansnom.wordpress.com" target="_blank">http://bonsansnom.wordpress.com</a>) - pieter@irail.be,</p>
+      <p>Copyright &copy; 2008, 2009, 2010, 2011 Yeri Tiete ("<a href="http://yeri.be" target="_blank">Tuinslak</a>") - yeri@irail.be,</p>
+      <p>Copyright &copy; 2010, 2011 Pieter Colpaert (<a href="http://bonsansnom.wordpress.com" target="_blank">http://bonsansnom.wordpress.com</a>) - pieter@irail.be,</p>
       <p>And <a href="http://project.irail.be/cgi-bin/trac.fcgi/wiki/Contributors">others</a>.</p>
+      <h1>Blog</h1>
+      <a href="http://blog.irail.be">Blog.iRail.be</a>
       <h1>Bugs</h1>
       <p>Nobody is perfect. Writing a webscraper is not the most fault tolerant system you would ever write. Since we think sooner or later NMBS will release an API of their own, we didn't mind writing clean code. So before you dive in, don't mind the mess.</p>
       <p>On the other hand if you noticed a flaw, something that doesn't work, something that might be important, ... feel free to report a new ticket at the <a href="http://project.irail.be/cgi-bin/trac.fcgi/newticket">project iRail</a> site.</p>
