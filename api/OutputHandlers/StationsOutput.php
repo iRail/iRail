1b159cbc5bed6b26b170b895ca7ec3e9153fc056
Pieter Colpaert <pieter@irail.be>
2010-12-05 21:54:03 +0100

    StationsOuput: if no lang attribute specified, return al languages

--- api/OutputHandlers/StationsOutput.php
+++ api/OutputHandlers/StationsOutput.php
@@ -10,6 +10,7 @@ include_once("Output.php");
 abstract class StationsOutput implements Output {
 
     protected $lang;
+
     function __construct($lang) {
         $this->lang = $lang;
     }
@@ -18,21 +19,38 @@ abstract class StationsOutput implements Output {
         $lang = $this->lang;
         $xml = new DOMDocument("1.0", "UTF-8");
         $rootNode = $xml->createElement("stations");
-        $rootNode ->setAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
-        $rootNode ->setAttribute("xsi:noNamespaceSchemaLocation", "stations.xsd");
+        $rootNode->setAttribute("xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
+        $rootNode->setAttribute("xsi:noNamespaceSchemaLocation", "stations.xsd");
         $rootNode->setAttribute("version", "1.0");
         $rootNode->setAttribute("timestamp", date("U"));
 
         $xml->appendChild($rootNode);
         foreach ($stationsarray as $stat) {
-            $station = $xml->createElement("station", $stat->getName($lang));
-            $station->setAttribute("id", $stat->getId());
-            //provide also this tag for old versions
-            $station->setAttribute("location", $stat->getY() . " " . $stat->getX());
-            //new version
-            $station->setAttribute("locationY", $stat->getY());
-            $station->setAttribute("locationX", $stat->getX());
-            $rootNode->appendChild($station);
+            if (!isset($_GET["lang"])) {
+                $previousname = array();
+                foreach ($stat->getNames() as $name) {
+                    if (!in_array($name, $previousname)) {
+                        $station = $xml->createElement("station", $name);
+                        $station->setAttribute("id", $stat->getId());
+                        //provide also this tag for old versions
+                        $station->setAttribute("location", $stat->getY() . " " . $stat->getX());
+                        //new version
+                        $station->setAttribute("locationY", $stat->getY());
+                        $station->setAttribute("locationX", $stat->getX());
+                        $rootNode->appendChild($station);
+                        $previousname[sizeof($previousname)] = $name;
+                    }
+                }
+            } else {
+                $station = $xml->createElement("station", $stat->getName($lang));
+                $station->setAttribute("id", $stat->getId());
+                //provide also this tag for old versions
+                $station->setAttribute("location", $stat->getY() . " " . $stat->getX());
+                //new version
+                $station->setAttribute("locationY", $stat->getY());
+                $station->setAttribute("locationX", $stat->getX());
+                $rootNode->appendChild($station);
+            }
         }
         return $xml;
     }
