7a686a6415982ebbf7e8803a9eba918dd27856a8
Pieter Colpaert <pieter@iRail.be>
2011-02-20 02:41:20 +0100

    changed iRail databasename into irail databasename

--- includes/footer.php
+++
@@ -1,2 +0,0 @@
-<p style="color:#999999;font-size:12px;padding:0 10px;">made by <a href="about">iRail team</a>;
-data from <a href="http://www.b-rail.be/" target="_blank">(B)</a>; <a href="http://project.iRail.be" target="_blank">Help out</a>.</p>
