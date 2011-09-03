1c7d3a47a787507eb3e8a46f111e11b920694eab
Pieter Colpaert <pieter@iRail.be>
2011-09-03 09:47:42 +0200

    fixed casting errors

--- api/data/NMBS/liveboard.php
+++ api/data/NMBS/liveboard.php
@@ -21,7 +21,7 @@ class liveboard{
         }else if(strtoupper(substr($request->getArrdep(), 0, 1)) == "D"){
             $html = liveboard::fetchData($dataroot->station, $request->getTime(), $request->getLang(),"D");
             $dataroot->departure = liveboard::parseData($html, $request->getTime(), $request->getLang());
-        }	       
+        }
         else{
             throw new Exception("Not a good timeSel value: try ARR or DEP", 300);
         }
@@ -35,13 +35,11 @@ class liveboard{
             "useragent" => $irailAgent,
         );
         $body = "";
-/*
+        /*
+          For example, run this in command line:
 
-For example, run this in command line:
-
-$ curl "http://hari.b-rail.be/Hafas/bin/stboard.exe/en?start=yes&time=15%3a12&date=01.09.2011&inputTripelId=A=1@O=@X=@Y=@U=80@L=008892007@B=1@p=@&maxJourneys=50&boardType=dep&hcount=1&htype=NokiaC7-00%2f022.014%2fsw_platform%3dS60%3bsw_platform_version%3d5.2%3bjava_build_version%3d2.2.54&L=vs_java3&productsFilter=00010000001111"
-
-*/	
+          $ curl "http://hari.b-rail.be/Hafas/bin/stboard.exe/en?start=yes&time=15%3a12&date=01.09.2011&inputTripelId=A=1@O=@X=@Y=@U=80@L=008892007@B=1@p=@&maxJourneys=50&boardType=dep&hcount=1&htype=NokiaC7-00%2f022.014%2fsw_platform%3dS60%3bsw_platform_version%3d5.2%3bjava_build_version%3d2.2.54&L=vs_java3&productsFilter=00010000001111"
+        */
         $scrapeUrl = "http://hari.b-rail.be/Hafas/bin/stboard.exe/en?start=yes";
         $hafasid = $station->getHID();
         //important TODO: date parameters - parse from URI first
@@ -66,15 +64,15 @@ $ curl "http://hari.b-rail.be/Hafas/bin/stboard.exe/en?start=yes&time=15%3a12&da
         $i = 0;
 
         $hour = substr($time,0,2);
-        $hour_ = substr($data->Journey[0]["fpTime"],0,2);
+        $hour_ = substr((string)$data->Journey[0]["fpTime"],0,2);
         $minutes = substr($time,3,2);
-        $minutes_ = substr($data->Journey[0]["fpTime"],3,2);
+        $minutes_ = substr((string)$data->Journey[0]["fpTime"],3,2);
         
         while(($hour_-$hour)*60 + ($minutes_ - $minutes) <= 60){
             $journey = $data->Journey[$i] ;
             
             $left = 0;
-            $delay = $journey["delay"];
+            $delay = (string)$journey["delay"];
             if($delay == "-"){
                 $delay="0";
             }
@@ -83,8 +81,8 @@ $ curl "http://hari.b-rail.be/Hafas/bin/stboard.exe/en?start=yes&time=15%3a12&da
             if(isset($journey["platform"])){
                 $platform = (string)$journey["platform"];
             }
-            $time = "00d" . $journey["fpTime"] . ":00";
-            preg_match("/(..)\/(..)\/(..)/si",$journey["fpDate"],$dayxplode);
+            $time = "00d" . (string)$journey["fpTime"] . ":00";
+            preg_match("/(..)\/(..)\/(..)/si",(string)$journey["fpDate"],$dayxplode);
 	    $dateparam = "20" . $dayxplode[3].$dayxplode[2].$dayxplode[1];
             
             $unixtime = tools::transformtime($time,$dateparam);
@@ -119,8 +117,8 @@ $ curl "http://hari.b-rail.be/Hafas/bin/stboard.exe/en?start=yes&time=15%3a12&da
 	    $nodes[$i]->platform->name = $platform;
 	    $nodes[$i]->platform->normal = $platformNormal;
 	    $nodes[$i]->left = $left;
-            $hour_ = substr($data->Journey[$i]["fpTime"],0,2);
-            $minutes_ = substr($data->Journey[$i]["fpTime"],3,2);
+            $hour_ = substr((string)$data->Journey[$i]["fpTime"],0,2);
+            $minutes_ = substr((string)$data->Journey[$i]["fpTime"],3,2);
             $i++;
 	}
         return $nodes;
