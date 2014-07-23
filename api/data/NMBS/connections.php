21c3a5e7e79e8c16283ce25221f5844c50c0b309
Pieter Colpaert <pieter.colpaert@okfn.org>
2014-07-23 14:37:34 +0200

    Fix hash

--- api/data/NMBS/connections.php
+++ api/data/NMBS/connections.php
@@ -208,9 +208,8 @@ class connections{
 					     $j++;
 					}else if($att->Attribute["type"] == "DIRECTION"){
                                             $__stat = new Station();
-                                            $__stat->name = trim($att->Attribute->AttributeVariant->Text);
                                             //This recently changed: only fetch direction name, nothing else.
-                                            $__stat = trim($att->Attribute->AttributeVariant->Text);
+                                            $__stat->name = trim($att->Attribute->AttributeVariant->Text);
                                             $__stat->name = str_replace("% [NMBS/SNCB]%","",$__stat->name);
                                             $directions[$k] = $__stat;
                                             $k++;
