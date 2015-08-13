ca8c59b122bcba5b27682737ccb15b8aefd8e820
Tjoosten <Tjoosten3@gmail.com>
2015-08-13 12:11:55 +0200

    reformat code -> output directory

--- api/output/Json.php
+++ api/output/Json.php
@@ -1,45 +1,50 @@
 <?php
-  /* Copyright (C) 2011 by iRail vzw/asbl */
-  /**
-   * Prints the Json style output
-   *
-   *
-   * @package output
-   */
+/* Copyright (C) 2011 by iRail vzw/asbl */
+/**
+ * Prints the Json style output
+ *
+ *
+ * @package output
+ */
 include_once("Printer.php");
-class Json extends Printer{
-     private $rootname;
 
-    function printHeader(){
-	  header("Access-Control-Allow-Origin: *");
-	  header("Content-Type: application/json;charset=UTF-8");
-     }
+class Json extends Printer
+{
+    private $rootname;
+
+    function printHeader()
+    {
+        header("Access-Control-Allow-Origin: *");
+        header("Content-Type: application/json;charset=UTF-8");
+    }
 
     /**
      * @param $ec
      * @param $msg
      */
-    function printError($ec, $msg){
-	  $this->printHeader();
-	  header("HTTP/1.1 $ec $msg");
-	  echo "{\"error\":$ec,\"message\":\"$msg\"}";
-     }
+    function printError($ec, $msg)
+    {
+        $this->printHeader();
+        header("HTTP/1.1 $ec $msg");
+        echo "{\"error\":$ec,\"message\":\"$msg\"}";
+    }
 
     /**
      * @param $name
      * @param $version
      * @param $timestamp
      */
-    function startRootElement($name, $version, $timestamp){
-	  $this->rootname = $name;
-	  echo "{\"version\":\"$version\",\"timestamp\":\"$timestamp\",";
-     }
+    function startRootElement($name, $version, $timestamp)
+    {
+        $this->rootname = $name;
+        echo "{\"version\":\"$version\",\"timestamp\":\"$timestamp\",";
+    }
 
 //make a stack of array information, always work on the last one
 //for nested array support
-     private $stack = [];
-     private $arrayindices = [];
-     private $currentarrayindex = -1;
+    private $stack = [];
+    private $arrayindices = [];
+    private $currentarrayindex = -1;
 
 
     /**
@@ -47,91 +52,99 @@ class Json extends Printer{
      * @param $number
      * @param bool $root
      */
-    function startArray($name,$number, $root = false){
-	  if(!$root || $this->rootname == "liveboard" || $this->rootname == "vehicleinformation"){
-	       echo "\"" . $name . "s\":{\"number\":\"$number\",";
-	  }
-	  echo "\"$name\":[";
-	  $this->currentarrayindex ++;
-	  $this->stack[$this->currentarrayindex] = $name;
-	  $this->arrayindices[$this->currentarrayindex] = 0;
-     }
+    function startArray($name, $number, $root = false)
+    {
+        if (!$root || $this->rootname == "liveboard" || $this->rootname == "vehicleinformation") {
+            echo "\"" . $name . "s\":{\"number\":\"$number\",";
+        }
+        echo "\"$name\":[";
+        $this->currentarrayindex++;
+        $this->stack[$this->currentarrayindex] = $name;
+        $this->arrayindices[$this->currentarrayindex] = 0;
+    }
 
-     function nextArrayElement(){
-	  echo ",";
-	  $this->arrayindices[$this->currentarrayindex]++;
-     }
+    function nextArrayElement()
+    {
+        echo ",";
+        $this->arrayindices[$this->currentarrayindex]++;
+    }
 
-     function nextObjectElement(){
-	  echo ",";
-     }
+    function nextObjectElement()
+    {
+        echo ",";
+    }
 
     /**
      * @param $name
      * @param $object
      */
-    function startObject($name, $object){
-	  if($this->currentarrayindex >-1 && $this->stack[$this->currentarrayindex] == $name){
-	       echo "{";
+    function startObject($name, $object)
+    {
+        if ($this->currentarrayindex > -1 && $this->stack[$this->currentarrayindex] == $name) {
+            echo "{";
 //show id (in array) except if array of stations (compatibility issues)
-	       if($name != "station"){
-		    echo "\"id\":\"".$this->arrayindices[$this->currentarrayindex]."\",";
-	       }
-	  }
-	  else{
-	       if($this->rootname != "stations" && $name == "station" || $name == "platform"){ // split station and platform into station/platform and stationinfo/platforminfox to be compatible with 1.0
-		    echo "\"$name\":\"$object->name\",";
-		    echo "\"". $name. "info\":{";
-	       }elseif($this->rootname != "vehicle" && $name == "vehicle"){ // split vehicle into vehicle and vehicleinfo to be compatible with 1.0
-		    echo "\"$name\":\"$object->name\",";
-		    echo "\"". $name. "info\":{";
-	       }else{
-		    echo "\"$name\":{";
-	       }
-	  }
-     }
+            if ($name != "station") {
+                echo "\"id\":\"" . $this->arrayindices[$this->currentarrayindex] . "\",";
+            }
+        } else {
+            if ($this->rootname != "stations" && $name == "station" || $name == "platform") { // split station and platform into station/platform and stationinfo/platforminfox to be compatible with 1.0
+                echo "\"$name\":\"$object->name\",";
+                echo "\"" . $name . "info\":{";
+            } elseif ($this->rootname != "vehicle" && $name == "vehicle") { // split vehicle into vehicle and vehicleinfo to be compatible with 1.0
+                echo "\"$name\":\"$object->name\",";
+                echo "\"" . $name . "info\":{";
+            } else {
+                echo "\"$name\":{";
+            }
+        }
+    }
 
     /**
      * @param $key
      * @param $val
      */
-    function startKeyVal($key,$val){
-	  echo "\"$key\":\"$val\"";
-     }
+    function startKeyVal($key, $val)
+    {
+        echo "\"$key\":\"$val\"";
+    }
 
     /**
      * @param $name
      * @param bool $root
      */
-    function endArray($name, $root = false){
-	  $this->stack[$this->currentarrayindex] = "";
-	  $this->arrayindices[$this->currentarrayindex] = 0;
-	  $this->currentarrayindex --;
-	  if($root && $this->rootname != "liveboard" && $this->rootname != "vehicleinformation"){
-	       echo "]";
-	  }else{
-	       echo "]}";
-	  }
-     }
+    function endArray($name, $root = false)
+    {
+        $this->stack[$this->currentarrayindex] = "";
+        $this->arrayindices[$this->currentarrayindex] = 0;
+        $this->currentarrayindex--;
+        if ($root && $this->rootname != "liveboard" && $this->rootname != "vehicleinformation") {
+            echo "]";
+        } else {
+            echo "]}";
+        }
+    }
 
     /**
      * @param $name
      */
-    function endObject($name){
-	  echo "}";
-     }
+    function endObject($name)
+    {
+        echo "}";
+    }
 
     /**
      * @param $name
      */
-    function endElement($name){
-	  
-     }
+    function endElement($name)
+    {
+
+    }
 
     /**
      * @param $name
      */
-    function endRootElement($name){
-	  echo "}";
-     }
+    function endRootElement($name)
+    {
+        echo "}";
+    }
 };
