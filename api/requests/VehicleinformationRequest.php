fcaffdd3d9d2c04c671470c3394970908561cba8
Tjoosten <Tjoosten3@gmail.com>
2015-08-15 20:04:02 +0200

    reformat code
    reformatted the code. And give a visibility attribute to a couple of
    functions.

--- api/requests/VehicleinformationRequest.php
+++ api/requests/VehicleinformationRequest.php
@@ -1,35 +1,40 @@
 <?php
-  /** Copyright (C) 2011 by iRail vzw/asbl 
-   *
-   * The request information for a vehicle lookup. It tracks the location and gets information about its future location.
-   *
-   * @author pieterc
-   */
+/** Copyright (C) 2011 by iRail vzw/asbl
+ *
+ * The request information for a vehicle lookup. It tracks the location and gets information about its future location.
+ *
+ * @author pieterc
+ */
 include_once("Request.php");
-class VehicleinformationRequest extends Request {
-     protected $id;
-     protected $fast;
 
-     function __construct() {
-	  parent::__construct();
-	  parent::setGetVar("id", "");
-          parent::setGetVar("fast", "false");
-	  parent::processRequiredVars(["id"]);
-     }
+class VehicleinformationRequest extends Request
+{
+    protected $id;
+    protected $fast;
 
-    /**
-     * @return mixed
-     */
-    public function getVehicleId() {
-	  return $this->id;
-     }
+    public function __construct()
+    {
+        parent::__construct();
+        parent::setGetVar("id", "");
+        parent::setGetVar("fast", "false");
+        parent::processRequiredVars(["id"]);
+    }
 
     /**
      * @return mixed
      */
-    public function getFast(){
-         return $this->fast;
-     }
-     
+    public function getVehicleId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFast()
+    {
+        return $this->fast;
+    }
 
 }
+
