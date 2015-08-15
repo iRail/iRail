12fa409a7b6cc00b29bd5c3d69313002c2a23d16
Tjoosten <Tjoosten3@gmail.com>
2015-08-15 18:07:37 +0200

    Improve output directory files.
    Changelog:
    
    - reformatted code
    - added @return attributes to the docblocks.
    - add visibility before the functions
    
    Note: I a step forwards to PSR-2.

--- api/output/Kml.php
+++ api/output/Kml.php
@@ -1,5 +1,5 @@
 <?php
-  /* Copyright (C) 2011 by iRail vzw/asbl */
+/* Copyright (C) 2011 by iRail vzw/asbl */
 include_once("Printer.php");
 
 /**
@@ -9,93 +9,112 @@ include_once("Printer.php");
  *
  * @package output
  */
-class Kml extends Printer{
-     private $ATTRIBUTES = ["id", "locationX", "locationY", "standardname", "left","delay", "normal"];
-     private $rootname;
+class Kml extends Printer
+{
+    private $ATTRIBUTES = ["id", "locationX", "locationY", "standardname", "left", "delay", "normal"];
+    private $rootname;
+
+    // make a stack of array information, always work on the last one
+    // for nested array support
+    private $stack = [];
+    private $arrayindices = [];
+    private $currentarrayindex = -1;
 
 
-     function printHeader(){
-	  header("Access-Control-Allow-Origin: *");
-	  header("Content-Type: application/vnd.google-earth.kml+xml");
-	  echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
-	  
-     }
+    public function printHeader()
+    {
+        header("Access-Control-Allow-Origin: *");
+        header("Content-Type: application/vnd.google-earth.kml+xml");
+        echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+    }
 
     /**
      * @param $ec
      * @param $msg
+     * @return mixed|void
      */
-    function printError($ec, $msg){
-	  $this->printHeader();
-	  header("HTTP/1.1 $ec $msg");
-	  echo "<error code=\"$ec\">$msg</error>";
-     }
+    public function printError($ec, $msg)
+    {
+        $this->printHeader();
+        header("HTTP/1.1 $ec $msg");
+        echo "<error code=\"$ec\">$msg</error>";
+    }
 
     /**
      * @param $name
      * @param $version
      * @param $timestamp
+     * @return mixed|void
      */
-    function startRootElement($name, $version, $timestamp){
-	  $this->rootname = $name;
-	  if($name == "stations"){
-	       echo "<kml xmlns=\"http://www.opengis.net/kml/2.2\">";
-	  }else{
-	       $this->printError(400,"KML only works for stations at this moment");
-	  }
-     }
-//make a stack of array information, always work on the last one
-//for nested array support
-     private $stack = [];
-     private $arrayindices = [];
-     private $currentarrayindex = -1;
+    public function startRootElement($name, $version, $timestamp)
+    {
+        $this->rootname = $name;
+        if ($name == "stations") {
+            echo "<kml xmlns=\"http://www.opengis.net/kml/2.2\">";
+        } else {
+            $this->printError(400, "KML only works for stations at this moment");
+        }
+    }
 
     /**
      * @param $name
      * @param $number
      * @param bool $root
+     * @return mixed|void
      */
-    function startArray($name,$number, $root = false){
-     }
+    public function startArray($name, $number, $root = false)
+    {
+    }
 
-     function nextArrayElement(){
-	  $this->arrayindices[$this->currentarrayindex]++;
-     }
+    public function nextArrayElement()
+    {
+        $this->arrayindices[$this->currentarrayindex]++;
+    }
 
     /**
      * @param $name
      * @param $object
+     * @return mixed|void
      */
-    function startObject($name, $object){
-	  if($name == "station"){
-	       echo "<Placemark id='". $object->id ."'><name>". $object->name ."</name><Point><coordinates>". $object->locationX .",". $object->locationY."</coordinates></Point></Placemark>";
-	  }
-     }
+    public function startObject($name, $object)
+    {
+        if ($name == "station") {
+            echo "<Placemark id='" . $object->id . "'><name>" . $object->name . "</name><Point><coordinates>" . $object->locationX . "," . $object->locationY . "</coordinates></Point></Placemark>";
+        }
+    }
 
     /**
      * @param $key
      * @param $val
+     * @return mixed|void
      */
-    function startKeyVal($key,$val){
-     }
+    public function startKeyVal($key, $val)
+    {
+    }
 
     /**
      * @param $name
+     * @return mixed|void
      */
-    function endElement($name){
-     }
+    public function endElement($name)
+    {
+    }
 
     /**
      * @param $name
      * @param bool $root
+     * @return mixed|void
      */
-    function endArray($name, $root = false){
-     }
+    public function endArray($name, $root = false)
+    {
+    }
 
     /**
      * @param $name
+     * @return mixed|void
      */
-    function endRootElement($name){
-	  echo "</kml>";
-     }
+    public function endRootElement($name)
+    {
+        echo "</kml>";
+    }
 };
