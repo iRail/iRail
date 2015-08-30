f199d3024f64ec80abc7ca0a341d01788f7adc42
Tim Joosten <Tjoosten@users.noreply.github.com>
2015-08-30 15:31:37 +0200

    Update connections.php

--- api/connections.php
+++ api/connections.php
@@ -29,10 +29,17 @@ include_once '../vendor/autoload.php';
 include_once 'APICall.php';
 date_default_timezone_set("Europe/Brussels");
 
-class ConnectionsCall extends APICall {
-    protected function logRequest() {
+class ConnectionsCall extends APICall 
+{
+    protected function logRequest() 
+    {
           $r = $this->request;
-	       parent::writeLog($_SERVER['HTTP_USER_AGENT'], $r->getFrom(), $r->getTo(), "none (connections)", $_SERVER['REMOTE_ADDR']);
+	  parent::writeLog(
+	  	$_SERVER['HTTP_USER_AGENT'],
+	  	$r->getFrom(), 
+	  	$r->getTo(), "none (connections)", 
+	  	$_SERVER['REMOTE_ADDR']
+	 );
     }
 }
 
