ae3b23c429affdd1763cf14e3a191c321cfd4998
Tjoosten <Tjoosten3@gmail.com>
2015-08-31 17:40:33 +0200

    Add description in docblock

--- api/data/structs/DepartureArrival.php
+++ api/data/structs/DepartureArrival.php
@@ -1,7 +1,8 @@
 <?php
 
 /** Copyright (C) 2011 by iRail vzw/asbl
- * This is the Connection class. It contains data.
+ *
+ * The datastruct for departure's and arrival's.
  *
  * @author pieterc
  */
