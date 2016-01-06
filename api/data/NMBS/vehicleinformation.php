80e3b678a0987f6ac4f0244c6923c9b9fe66ec00
Jan Cornelissen <info@jancornelissen.com>
2016-01-06 21:03:05 +0100

    Fix for missing delay/canceled info on last stop

--- api/data/NMBS/vehicleinformation.php
+++ api/data/NMBS/vehicleinformation.php
@@ -129,7 +129,16 @@ class vehicleinformation
                 // Time
                 $timenodearray = $node->children[1]->find('span');
                 $arriveTime = reset($timenodearray[0]->nodes[0]->_);
-                $departureTime = count($nodes[$i]->children[1]->children) == 3 ? reset($nodes[$i]->children[1]->children[2]->nodes[0]->_) : $arriveTime;
+                $departureTime = "";
+                
+                // Handle last stop time, delay and canceled info
+                if (count($nodes[$i]->children[1]->children) == 3) { 
+                    $departureTime = reset($nodes[$i]->children[1]->children[2]->nodes[0]->_);
+                } else {
+                    $departureTime = $arriveTime;
+                    $departureDelay = $arrivalDelay;
+                    $departureCanceled = $arrivalCanceled;
+                }
 
                 if (count($node->children[3]->find('a'))) {
                     $as = $node->children[3]->find('a');
