5543532e128a7446cf896eca2e3b45feaa209d9d
Karel Vervaeke <karel.vervaeke@aca-it.be>
2016-03-06 20:47:49 +0100

    Return logs as json array
    fixes #152

--- api/logs.php
+++ api/logs.php
@@ -1,5 +1,12 @@
 <?php
 include_once '../vendor/autoload.php';
+
+use IRail\JsonLog;
+
 header('Expires: '.gmdate('D, d M Y H:i:s \G\M\T', time() + (1)));//expires in 1 second
-header('Content-Type: text/plain');
-echo shell_exec('tail -n 1000 ../storage/irapi.log');
+header('Content-Type: application/json');
+$log = new JsonLog('../storage/irapi.log');
+
+print json_encode($log->getLastEntries(1000));
+
+?>
