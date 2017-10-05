db4f37e97a47ad9ab5ce7e2521f8596394f78392
Bertware <bert.marcelis@outlook.com>
2017-10-05 11:03:24 +0200

    Fix if-none-match header having no effect when the etag is prefixed with "w/"

--- api/output/Printer.php
+++ api/output/Printer.php
@@ -29,7 +29,7 @@ abstract class Printer
         $this->printCacheHeaders($etag);
 
         $headers = $this->getallheaders();
-        if (key_exists('If-None-Match', $headers) && $headers['If-None-Match'] == '"' . $etag . '"') {
+        if (key_exists('If-None-Match', $headers) && ($headers['If-None-Match'] == '"' . $etag . '"' ||$headers['If-None-Match'] == 'W/"' . $etag . '"')) {
             // Print the unchanged response code. Don't transmit a body
             http_response_code("304");
             return;
