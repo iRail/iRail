5de48a157983fd5f7f4cf9c10c2daf9dfef2208a
Bertware <bert.marcelis@outlook.com>
2020-01-05 20:00:42 +0100

    Better naming for line delimiter character variable in the disturbances endpoint #290

--- api/requests/DisturbancesRequest.php
+++ api/requests/DisturbancesRequest.php
@@ -7,16 +7,16 @@ class DisturbancesRequest extends Request
     // Disturbances don't need a special request. This file makes disturbances fit in nicely with the framework.
     // Currently used fields: format & lang, both implemented in request.
 
-    protected $linebreaks;
+    protected $lineBreakCharacter;
 
     public function __construct()
     {
         parent::__construct();
-        parent::setGetVar('linebreaks', '');
+        parent::setGetVar('lineBreakCharacter', '');
     }
 
     public function getLinebreakCharacter(): string
     {
-        return strip_tags(urldecode($this->linebreaks), '<br>');
+        return strip_tags(urldecode($this->lineBreakCharacter), '<br>');
     }
 }
