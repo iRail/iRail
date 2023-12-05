b75bb4650685fa1f5924901143f32b8e6b25061c
Bert <bert.marcelis@outlook.com>
2023-12-05 23:40:17 +0100

    Clarify order of operations

--- src/api/data/NMBS/VehicleDatasource.php
+++ src/api/data/NMBS/VehicleDatasource.php
@@ -246,7 +246,7 @@ class VehicleDatasource
         curl_setopt($ch, CURLOPT_USERAGENT, $request_options['useragent']);
         curl_setopt($ch, CURLOPT_REFERER, $request_options['referer']);
         curl_setopt($ch, CURLOPT_TIMEOUT, $request_options['timeout']);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, ['x-api-key: ' . getenv('NMBS_API_KEY') ?? self::API_KEY]);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['x-api-key: ' . (getenv('NMBS_API_KEY') ?? self::API_KEY)]);
 
         $response = curl_exec($ch);
 
