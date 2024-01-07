3ae8fbf04fc4832f4e274c0d0888213f29c449db
Bert Marcelis <bert.marcelis@outlook.com>
2024-01-07 15:25:48 +0100

    Fix incorrect parameter type

--- app/Models/Dto/v2/DatedVehicleJourneyV2Converter.php
+++ app/Models/Dto/v2/DatedVehicleJourneyV2Converter.php
@@ -14,20 +14,20 @@ class DatedVehicleJourneyV2Converter extends V2Converter
      * @param IrailHttpRequest           $request
      * @param VehicleJourneySearchResult $result
      * @param TrainComposition[]         $composition
-     * @param CompositionStatistics      $compositionStatistics
+     * @param CompositionStatistics[] $compositionStatistics
      * @return array
      */
     public static function convert(
         IrailHttpRequest $request,
         VehicleJourneySearchResult $result,
         array $composition,
-        CompositionStatistics $compositionStatistics
+        array $compositionStatistics
     ): array {
         return [
             'vehicle'               => self::convertVehicle($result->getVehicle()),
             'stops'                 => array_map(fn($obj) => self::convertDepartureAndArrival($obj), $result->getStops()),
             'composition'           => array_map(fn($segment) => self::convertComposition($segment), $composition),
-            'compositionStatistics' => self::convertCompositionStats($compositionStatistics)
+            'compositionStatistics' => array_map(fn($statistics) => self::convertCompositionStats($statistics), $compositionStatistics)
         ];
     }
 
