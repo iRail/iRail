dfc6742c3701ed9261d5203f635c722da88f9c4a
Bert Marcelis <bert.marcelis@outlook.com>
2024-03-09 15:57:22 +0100

    Shows more cache statistics on debug page

--- public/cache-status.php
+++ public/cache-status.php
@@ -1,16 +1,20 @@
 <?php
 $status = opcache_get_status();
 
-echo 'Opcache Cached scripts: ' . $status['opcache_statistics']['num_cached_scripts'] . PHP_EOL;
-echo 'Opcache Cache hits: ' . $status['opcache_statistics']['hits'] . PHP_EOL;
-echo 'Opcache Cache misses: ' . $status['opcache_statistics']['misses'] . PHP_EOL;
-echo 'Opcache Memory usage: ' . $status['memory_usage']['used_memory'] / (1024 * 1024) . ' MB' . PHP_EOL;
+echo 'Opcache Cached scripts: ' . $status['opcache_statistics']['num_cached_scripts'] . '<br>';
+echo 'Opcache Cache hits: ' . $status['opcache_statistics']['hits'] . '<br>';
+echo 'Opcache Cache misses: ' . $status['opcache_statistics']['misses'] . '<br>';
+echo 'Opcache Memory usage: ' . $status['memory_usage']['used_memory'] / (1024 * 1024) . ' MB' . '<br>';
 
 if (apcu_enabled()) {
     $apcu = apcu_cache_info();
-    echo 'APCU hits:' . $apcu['num_hits'] . PHP_EOL;
-    echo 'APCU misses:' . $apcu['num_misses'] . PHP_EOL;
-    echo 'APCU cache list size:' . count($apcu['cache_list']) . PHP_EOL;
+    echo 'APCU hits:' . $apcu['num_hits'] . '<br>';
+    echo 'APCU misses:' . $apcu['num_misses'] . '<br>';
+    echo 'APCU cache list size:' . count($apcu['cache_list']) . '<br>';
+    echo '<br>';
+    foreach ($apcu['cache_list'] as $item) {
+        echo $item['info'] . '    hits:' . $item['num_hits'] . '<br>';
+    }
 } else {
     echo 'APCU not enabled';
 }
