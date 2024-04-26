639b72dea96fad9770eb05ffff143b318a04c2f9
Bert Marcelis <bert.marcelis@outlook.com>
2024-04-26 21:09:09 +0200

    Rate limit per hour with bursts per 10 second

--- app/Http/Middleware/RateLimitMiddleware.php
+++ app/Http/Middleware/RateLimitMiddleware.php
@@ -4,6 +4,7 @@ namespace Irail\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\RateLimiter;
 use Irail\Exceptions\IrailHttpException;
 
@@ -16,18 +17,49 @@ class RateLimitMiddleware
      * @param Closure $next
      * @return mixed
      */
-    public function handle(Request $request, Closure $next): mixed
+    public function handle(Request $request, Closure $next): Response
+    {
+        return $this->rateLimitedLongTerm($request, $next);
+    }
+
+    /**
+     * @param Request $request
+     * @param Closure $next
+     * @return Response
+     */
+    public function rateLimitedLongTerm(Request $request, Closure $next): Response
     {
         $response = RateLimiter::attempt(
-            'request_rate|' . $request->getClientIp(),
-            45,
-            function () use ($next, $request) {
-                return $next($request);
+            'request_rate|long-term|' . $request->getClientIp(),
+            1800,
+            function () use ($request, $next) {
+                return $this->rateLimitedShortTerm($request, $next);
             },
-            15 // 15 seconds buckets, so 3 requests per second for each 15 seconds
+            3600 // 1800 requests per hour, one per 2 seconds
         );
         if ($response === false) {
-            throw new IrailHttpException(429, 'Too many requests');
+            throw new IrailHttpException(429, 'Too many requests (long-term)');
+        }
+        return $response;
+    }
+
+    /**
+     * @param Request $request
+     * @param Closure $next
+     * @return Response
+     */
+    public function rateLimitedShortTerm(Request $request, Closure $next): Response
+    {
+        $response = RateLimiter::attempt(
+            'request_rate|burst|' . $request->getClientIp(),
+            30,
+            function () use ($request, $next) {
+                return $next($request);
+            },
+            10 // 30 request per 10 seconds, 3 per second bursts
+        );
+        if ($response === false) {
+            throw new IrailHttpException(429, 'Too many requests (long-term)');
         }
         return $response;
     }
