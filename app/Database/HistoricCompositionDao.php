8b22f8deed440e943e947cb967d0bee070ab160f
Bert <bert.marcelis@outlook.com>
2024-08-23 18:09:24 +0200

    Improve resilience when database becomes unavailable

--- app/Database/HistoricCompositionDao.php
+++ app/Database/HistoricCompositionDao.php
@@ -160,27 +160,31 @@ class HistoricCompositionDao
             $this->deleteCompositionHistory($compositionId);
         }
 
-        $units = $composition->getUnits();
-        $passengerCarriageCount = $this->countPassengerCarriages($units);
-        $typesFrequency = array_count_values(array_map(fn ($unit) => $unit->getMaterialType()->getParentType(), $units));
-        $primaryMaterialType = array_keys($typesFrequency, max($typesFrequency))[0];
-        $compositionId = $this->insertComposition($composition, $primaryMaterialType, $passengerCarriageCount);
-        Log::debug("Recording historic composition for journey {$composition->getVehicle()->getId()}");
-        foreach ($units as $position => $unit) {
-            if (!($unit instanceof TrainCompositionUnitWithId)) {
-                // TODO: When reading these compositions back, the difference between the summary and stored data should be detected
-                Log::info("Cannot record historic composition due to missing material ids for journey {$composition->getVehicle()->getId()} reported by {$composition->getCompositionSource()}");
-                continue;
-            }
+        try {
+            $units = $composition->getUnits();
+            $passengerCarriageCount = $this->countPassengerCarriages($units);
+            $typesFrequency = array_count_values(array_map(fn($unit) => $unit->getMaterialType()->getParentType(), $units));
+            $primaryMaterialType = array_keys($typesFrequency, max($typesFrequency))[0];
+            $compositionId = $this->insertComposition($composition, $primaryMaterialType, $passengerCarriageCount);
+            Log::debug("Recording historic composition for journey {$composition->getVehicle()->getId()}");
+            foreach ($units as $position => $unit) {
+                if (!($unit instanceof TrainCompositionUnitWithId)) {
+                    // TODO: When reading these compositions back, the difference between the summary and stored data should be detected
+                    Log::info("Cannot record historic composition due to missing material ids for journey {$composition->getVehicle()->getId()} reported by {$composition->getCompositionSource()}");
+                    continue;
+                }
 
-            $this->insertIfNotExists($unit);
-            DB::update('INSERT INTO composition_unit_usage(uic_code, historic_composition_id, position) VALUES (?,?,?)', [
-                $unit->getUicCode(),
-                $compositionId,
-                $position + 1
-            ]);
+                $this->insertIfNotExists($unit);
+                DB::update('INSERT INTO composition_unit_usage(uic_code, historic_composition_id, position) VALUES (?,?,?)', [
+                    $unit->getUicCode(),
+                    $compositionId,
+                    $position + 1
+                ]);
+            }
+            $this->markAsRecorded($composition);
+        } catch (\Exception $e) {
+            Log::error("Failed to record composition for journey {$composition->getVehicle()->getId()}: {$e->getMessage()}\n {$e->getTraceAsString()}");
         }
-        $this->markAsRecorded($composition);
     }
 
 
