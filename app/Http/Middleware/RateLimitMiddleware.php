efb1ea93a5c327ead24666a43f9135429c552131
Bert <bert.marcelis@outlook.com>
2024-11-29 10:20:31 +0100

    Add possibility to permanently block IPs

--- app/Http/Middleware/RateLimitMiddleware.php
+++ app/Http/Middleware/RateLimitMiddleware.php
@@ -5,6 +5,7 @@ namespace Irail\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\RateLimiter;
 use Irail\Exceptions\IrailHttpException;
 use Irail\Util\InMemoryMetrics;
@@ -35,6 +36,12 @@ class RateLimitMiddleware
         if ($request->hasHeader('do-connecting-ip')) {
             // Read from the right HTTP header when deployed on DigitalOcean App Platform
             $clientIp = $request->header('do-connecting-ip');
+        }
+
+        $blockedIps = getenv('IP_BLOCKLIST');
+        if (!empty($blockedIps) && in_array($clientIp, explode(",",$blockedIps))) {
+            Log::info("Blocked client ". $clientIp . ", present on blocklist");
+            throw new IrailHttpException(429, 'Too many requests (permanent blocklist)');
         }
 
         $response = RateLimiter::attempt(
