1b4ff875eceb4e444e5a78bae4e83f12fcb367f3
Bert <bert.marcelis@outlook.com>
2026-02-23 00:04:33 +0100

    Add redirect to docs.irail.be for requests to root domain

---
+++ src/main/java/be/irail/api/controllers/HomeRedirectController.java
@@ -0,0 +1,25 @@
+package be.irail.api.controllers;
+
+import be.irail.api.config.Metrics;
+import com.codahale.metrics.Meter;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+import org.springframework.stereotype.Component;
+
+import java.net.URI;
+
+@Component
+@Path("/")
+public class HomeRedirectController {
+
+    private final Meter redirectMeter = Metrics.getRegistry().meter("root-docs-redirect");
+
+    @GET
+    @Path("/stations")
+    public Response redirectStations() {
+        redirectMeter.mark();
+        return Response.seeOther(URI.create("https://docs.irail.be")).build();
+    }
+
+}
