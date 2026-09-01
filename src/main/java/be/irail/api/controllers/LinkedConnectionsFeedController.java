9b1f0773e60d05a6d338d0ee28ed3270b7a66658
Pieter Colpaert <pieter.colpaert@ugent.be>
2026-09-01 13:24:26 +0200

    CORS on feed

--- src/main/java/be/irail/api/controllers/LinkedConnectionsFeedController.java
+++ src/main/java/be/irail/api/controllers/LinkedConnectionsFeedController.java
@@ -3,6 +3,7 @@ package be.irail.api.controllers;
 import be.irail.api.linkedconnections.LinkedConnectionsFeedService;
 import be.irail.api.linkedconnections.LinkedConnectionsService;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.OPTIONS;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.*;
@@ -15,7 +16,11 @@ import java.time.Instant;
 @Component
 @Path("/v1/feed")
 public class LinkedConnectionsFeedController {
-    private static final String CORS_HEADER = "Access-Control-Allow-Origin";
+    private static final String ALLOW_ORIGIN = "Access-Control-Allow-Origin";
+    private static final String ALLOW_METHODS = "Access-Control-Allow-Methods";
+    private static final String ALLOW_HEADERS = "Access-Control-Allow-Headers";
+    private static final String EXPOSE_HEADERS = "Access-Control-Expose-Headers";
+    private static final String MAX_AGE = "Access-Control-Max-Age";
     private final LinkedConnectionsFeedService service;
 
     public LinkedConnectionsFeedController(LinkedConnectionsFeedService service) {
@@ -35,13 +40,26 @@ public class LinkedConnectionsFeedController {
         return addHeaders(Response.ok(jsonLd, LinkedConnectionsService.JSON_LD_MEDIA_TYPE), entityTag).build();
     }
 
+    @OPTIONS
+    public Response options() {
+        return addCorsHeaders(Response.noContent()).build();
+    }
+
     private Response.ResponseBuilder addHeaders(Response.ResponseBuilder response, EntityTag entityTag) {
         CacheControl cacheControl = new CacheControl();
         cacheControl.setMaxAge(15);
         cacheControl.setPrivate(false);
-        return response.cacheControl(cacheControl)
+        return addCorsHeaders(response.cacheControl(cacheControl)
                 .tag(entityTag)
-                .header(CORS_HEADER, "*")
+                .header(EXPOSE_HEADERS, HttpHeaders.ETAG));
+    }
+
+    private Response.ResponseBuilder addCorsHeaders(Response.ResponseBuilder response) {
+        return response
+                .header(ALLOW_ORIGIN, "*")
+                .header(ALLOW_METHODS, "GET, OPTIONS")
+                .header(ALLOW_HEADERS, "Accept, If-None-Match")
+                .header(MAX_AGE, "86400")
                 .header(HttpHeaders.VARY, HttpHeaders.ACCEPT);
     }
 }
